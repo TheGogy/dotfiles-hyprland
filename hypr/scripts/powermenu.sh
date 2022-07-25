aa7db0fb98d1adacd5841f6b22ad4a7c2ef4534b
TheGogy <102483727+TheGogy@users.noreply.github.com>
2022-07-24 22:33:28 -0400

    Update powermenu.sh

--- hypr/scripts/powermenu.sh
+++ hypr/scripts/powermenu.sh
@@ -8,7 +8,7 @@ case $selected in
   logout)
     pkill -KILL -u gogy;;
   lock)
-    exec swaylock -f -r -l -L -s fill -i /home/gogy/.config/hypr/wallpapers/anime_girl_wallpaper.png;;
+    exec swaylock -f -r -l -L -s fill -i ~/.config/hypr/wallpapers/anime_girl_wallpaper.png;;
   reboot)
     exec systemctl reboot;;
   shutdown)
